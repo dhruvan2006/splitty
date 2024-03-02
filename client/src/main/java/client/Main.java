b38e7967d42c45a87c46df1686e2d2cfdf49c3c7
Efe Ayna <e.ayna@student.tudelft.nl>
2024-03-02 21:22:30 +0100

    template for StartScreen in JavaFx

--- client/src/main/java/client/Main.java
+++ client/src/main/java/client/Main.java
@@ -20,6 +20,7 @@ import static com.google.inject.Guice.createInjector;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
+import client.scenes.StartScreenCtrl;
 import com.google.inject.Injector;
 
 import client.scenes.AddQuoteCtrl;
@@ -37,13 +38,21 @@ public class Main extends Application {
         launch();
     }
 
-    @Override
-    public void start(Stage primaryStage) throws IOException {
+
+    /*public void start(Stage primaryStage) throws IOException {
 
         var overview = FXML.load(QuoteOverviewCtrl.class, "client", "scenes", "QuoteOverview.fxml");
         var add = FXML.load(AddQuoteCtrl.class, "client", "scenes", "AddQuote.fxml");
-
         var mainCtrl = INJECTOR.getInstance(MainCtrl.class);
         mainCtrl.initialize(primaryStage, overview, add);
+    }*/
+
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        var start = FXML.load(StartScreenCtrl.class, "client", "scenes", "StartScreen.fxml");
+        var mainCtrl = INJECTOR.getInstance(MainCtrl.class);
+        mainCtrl.initialize2(primaryStage, start);
     }
 }
