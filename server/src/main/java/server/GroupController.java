da111fbe5f41822e22f69e6e72a24a0ab1dfc20a
Octavian Apolozan <o.a.apolozan@student.tudelft.nl>
2024-03-04 00:59:37 +0100

    Update file GroupController.java

--- server/src/main/java/server/GroupController.java
+++ server/src/main/java/server/GroupController.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
 
 import server.database.GroupRepository;
 import commons.Group;
