35e43cafafab46ec6334c2a4f5c8f360b5177365
Armand <armand.ayar@outlook.com>
2024-03-03 15:37:57 +0100

    Add controller and repository

---
+++ server/src/main/java/server/api/ExpenseController.java
@@ -0,0 +1,42 @@
+package server.api;
+
+import commons.Expense;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import server.database.ExpenseRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/expense")
+public class ExpenseController {
+    private final ExpenseRepository repo;
+
+    public ExpenseController(ExpenseRepository repo){
+        this.repo = repo;
+    }
+
+    @GetMapping(path = { "", "/" })
+    public List<Expense> getAll() {
+        return repo.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Expense> getById(@PathVariable("id") long id) {
+        if (id < 0 || !repo.existsById(id)) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(repo.findById(id).get());
+    }
+
+    @PostMapping(path = { "add", "/" })
+    public ResponseEntity<Expense> add(@RequestBody Expense expense) {
+        Expense saved = repo.save(expense);
+        return ResponseEntity.ok(saved);
+    }
+}
