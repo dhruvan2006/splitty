3dfb723a953e8e2d0985da259695ae41582e6c17
Octavian Apolozan <o.a.apolozan@student.tudelft.nl>
2024-03-04 02:03:31 +0100

    Update file GroupController.java

--- server/src/main/java/server/GroupController.java
+++ server/src/main/java/server/GroupController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
