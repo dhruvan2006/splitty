206f19daac0405c005ea9caa4fdf78df1fd16437
Octavian Apolozan <o.a.apolozan@student.tudelft.nl>
2024-03-04 02:05:01 +0100

    Update file GroupController.java

--- server/src/main/java/server/GroupController.java
+++ server/src/main/java/server/GroupController.java
@@ -1,9 +1,7 @@
 package server.api;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
