066ee03c21a3c29dd139402981d08ab877461494
Octavian Apolozan <o.a.apolozan@student.tudelft.nl>
2024-03-09 13:36:54 +0100

    Update file ParticipantRepository.java

--- server/src/main/java/server/database/ParticipantRepository.java
+++ server/src/main/java/server/database/ParticipantRepository.java
@@ -1,6 +1,8 @@
 package server.database;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 
 import commons.Participant;
 
