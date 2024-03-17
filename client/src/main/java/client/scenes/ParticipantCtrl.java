23a0f0c61df4106c3dc3e1e1adbd602444d02317
Armand <a.ayar@student.tudelft.nl>
2024-03-17 12:49:13 +0100

    Made fxml for add/edit participant page

---
+++ client/src/main/java/client/scenes/ParticipantCtrl.java
@@ -0,0 +1,14 @@
+package client.scenes;
+
+import javafx.fxml.FXML;
+
+public class ParticipantCtrl {
+    @FXML
+    private void Cancel() {
+        System.out.println("Configuring Participant Canceled");
+    }
+    @FXML
+    private void Finish(){
+        System.out.println("Configuring Participant Finished");
+    }
+}
