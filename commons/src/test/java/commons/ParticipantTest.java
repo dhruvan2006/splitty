32b8cd9c216efa3133810a91cda686558dccee7e
Efe Ayna <e.ayna@student.tudelft.nl>
2024-03-20 23:31:39 +0100

    fixing pipeline

--- commons/src/test/java/commons/ParticipantTest.java
+++ commons/src/test/java/commons/ParticipantTest.java
@@ -1,6 +1,5 @@
 package commons;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 public class ParticipantTest {
