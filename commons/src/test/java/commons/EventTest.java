5224e1be285810f46048802f7b42a1ee1cad4f33
Octavian Apolozan <o.a.apolozan@student.tudelft.nl>
2024-03-20 19:58:28 +0100

    Update 2 files
    - /commons/src/test/java/commons/GroupTest.java
    - /commons/src/test/java/commons/EventTest.java

--- commons/src/test/java/commons/EventTest.java
+++ commons/src/test/java/commons/EventTest.java
@@ -2,8 +2,7 @@ package commons;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
--- commons/src/test/java/commons/GroupTest.java
+++ commons/src/test/java/commons/GroupTest.java
@@ -2,6 +2,7 @@ package commons;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
