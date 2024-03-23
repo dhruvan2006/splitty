7fc663a997a2450634a40ada7c90b1d9f1bb81a8
Efe Ayna <e.ayna@student.tudelft.nl>
2024-03-23 13:47:21 +0100

    changes to requests

--- server/src/main/java/server/database/EventRepository.java
+++ server/src/main/java/server/database/EventRepository.java
@@ -5,6 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EventRepository extends JpaRepository<Event, Long>{
-    boolean existsByInviteCode(String inviteCode);
-}
+public interface EventRepository extends JpaRepository<Event, Long>{ }
