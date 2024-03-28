cfb93fe8728da69f411477945d40c9bbadcc8f56
Octavian Apolozan <o.a.apolozan@student.tudelft.nl>
2024-03-28 17:07:05 +0100

    Small change

--- commons/src/main/java/commons/Event.java
+++ commons/src/main/java/commons/Event.java
@@ -3,9 +3,7 @@ package commons;
 import jakarta.persistence.*;
 import org.apache.commons.lang3.RandomStringUtils;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Entity
 public class Event {
