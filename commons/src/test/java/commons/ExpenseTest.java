d050f0b59aa73477be4ef6548727cbb1d1298b47
Octavian Apolozan <o.a.apolozan@student.tudelft.nl>
2024-03-28 18:10:31 +0100

    Small typo

--- commons/src/test/java/commons/ExpenseTest.java
+++ commons/src/test/java/commons/ExpenseTest.java
@@ -7,6 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.*;
+
 class ExpenseTest {
     @Test
     void testConstructor() {
