3844b201f04e20b9dad63c17e05f4278e0fe5285
Efe Ayna <e.ayna@student.tudelft.nl>
2024-03-30 16:21:04 +0100

    fixing pipeline

--- server/src/main/java/server/api/MainController.java
+++ server/src/main/java/server/api/MainController.java
@@ -1,15 +1,10 @@
 package server.api;
 
-import commons.PasswordGenerator;
-import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.*;
-import commons.Participant;
 
-import java.util.Random;
 
 @Controller
 @RequestMapping("/")
