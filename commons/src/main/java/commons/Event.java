dd339eab777b12e1fbb77d73a8968b2aec0e9525
Dhruvan Ganadhandayuthapani <dhruvan2006@gmail.com>
2024-03-31 18:45:18 +0200

    remove id from invite code generator

--- commons/src/main/java/commons/Event.java
+++ commons/src/main/java/commons/Event.java
@@ -34,7 +34,7 @@ public class Event {
     }
 
     private String generateInviteCode() {
-        return this.id + RandomStringUtils.randomAlphanumeric(4).toUpperCase();
+        return RandomStringUtils.randomAlphanumeric(6).toUpperCase();
     }
 
     public long getId() {
