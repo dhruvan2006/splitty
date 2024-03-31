27160a12627d136d44cb4f040eaa44a4fb91b618
Efe Ayna <e.ayna@student.tudelft.nl>
2024-03-31 18:12:01 +0200

    fixing pipeline

--- client/src/main/java/client/utils/ServerUtils.java
+++ client/src/main/java/client/utils/ServerUtils.java
@@ -23,7 +23,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.sql.Date;
 import java.util.List;
 
 import commons.Event;
