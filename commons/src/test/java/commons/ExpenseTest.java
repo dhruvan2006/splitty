96e71bb5cc49a803f0168b87886f152155417434
Dhruvan Ganadhandayuthapani <dhruvan2006@gmail.com>
2024-04-05 12:47:58 +0200

    Fix checkstyle

--- commons/src/test/java/commons/ExpenseTest.java
+++ commons/src/test/java/commons/ExpenseTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.*;
 
