6dd2562ee6bb1cfb12ccb522b9fde43613dfb992
Octavian Apolozan <o.a.apolozan@student.tudelft.nl>
2024-04-06 22:10:45 +0200

    Forgotten imports for the tests

--- server/src/test/java/server/api/EventControllerTest.java
+++ server/src/test/java/server/api/EventControllerTest.java
@@ -25,6 +25,10 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.context.request.async.DeferredResult;
 
 public class EventControllerTest {
 
