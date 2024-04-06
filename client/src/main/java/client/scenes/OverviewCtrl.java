44d8dcb2e74a83091717dec2d90e817d0e94124f
Armand <a.ayar@student.tudelft.nl>
2024-04-06 13:33:15 +0200

    Remove unused imports and unnecessary tests

--- client/src/main/java/client/scenes/OverviewCtrl.java
+++ client/src/main/java/client/scenes/OverviewCtrl.java
@@ -25,8 +25,6 @@ import javafx.util.Pair;
 
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class OverviewCtrl implements Initializable {
