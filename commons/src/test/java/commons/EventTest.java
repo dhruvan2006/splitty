14f74611565e46fc9781fe160cb679e673106af1
Dhruvan Ganadhandayuthapani <dhruvan2006@gmail.com>
2024-04-14 13:17:09 +0200

    Tests for Event

--- commons/src/test/java/commons/EventTest.java
+++ commons/src/test/java/commons/EventTest.java
@@ -123,4 +123,113 @@ class EventTest {
         Event e = new Event("x");
         assertTrue(e.toString().contains("x (code: "));
     }
+
+    @Test
+    void testUpdateParticipant() {
+        Event e = new Event();
+        e.setParticipants(new ArrayList<>());
+        Participant p1 = new Participant();
+        p1.setId(1L);
+        e.addParticipant(p1);
+        Participant p2 = new Participant();
+        p2.setUserName("Hello");
+        assertTrue(e.updateParticipant(1L, p2));
+        assertEquals("Hello", e.getParticipants().get(0).getUserName());
+    }
+
+    @Test
+    void testUpdateParticipantFailure() {
+        Event e = new Event();
+        e.setParticipants(new ArrayList<>());
+        Participant p1 = new Participant();
+        p1.setUserName("Hello2");
+        assertFalse(e.updateParticipant(3, p1));
+    }
+
+    @Test
+    void testCalculateIndividualDebt() {
+        Event e = new Event();
+        e.setId(1L);
+        Participant p1 = new Participant();
+        p1.setId(1L);
+        Participant p2 = new Participant();
+        p2.setId(2L);
+        e.setParticipants(new ArrayList<>());
+        e.addParticipant(p1);
+        e.addParticipant(p2);
+
+        e.setExpenses(new ArrayList<>());
+        e.getExpenses().add(new Expense("Title", 100, p1, e));
+
+        Map<Participant, Map<Participant, Integer>> debts = e.calculateIndividualDebt();
+
+        assertNotNull(debts.get(p2));
+        assertEquals(50, (int) debts.get(p2).get(p1));
+        assertNull(debts.get(p1).get(p2));
+    }
+
+    @Test
+    void testCalculateOptimizedDebts1() {
+        Event e = new Event();
+        e.setId(1L);
+        Participant p1 = new Participant();
+        p1.setId(1L);
+        Participant p2 = new Participant();
+        p2.setId(2L);
+        Participant p3 = new Participant();
+        p3.setId(3L);
+        e.setParticipants(new ArrayList<>());
+        e.addParticipant(p1);
+        e.addParticipant(p2);
+        e.addParticipant(p3);
+
+        e.setExpenses(new ArrayList<>());
+        e.getExpenses().add(new Expense("Title", 100, p1, e));
+
+        Map<Participant, Map<Participant, Integer>> debts = e.calculateOptimizedDebts();
+
+        assertNotNull(debts.get(p2));
+        assertEquals(33, (int) debts.get(p2).get(p1));
+        assertEquals(33, (int) debts.get(p3).get(p1));
+    }
+
+    @Test
+    void testCalculateOptimizedDebts2() {
+        Event e = new Event();
+        e.setId(1L);
+        Participant p1 = new Participant();
+        p1.setId(1L);
+        Participant p2 = new Participant();
+        p2.setId(2L);
+        Participant p3 = new Participant();
+        p3.setId(3L);
+        e.setParticipants(new ArrayList<>());
+        e.addParticipant(p1);
+        e.addParticipant(p2);
+        e.addParticipant(p3);
+
+        e.setExpenses(new ArrayList<>());
+        e.getExpenses().add(new Expense("Title", 300, p1, e));
+        e.getExpenses().add(new Expense("Title", 150, p2, e));
+
+        Map<Participant, Map<Participant, Integer>> debts = e.calculateOptimizedDebts();
+
+        assertTrue(debts.get(p1).isEmpty());
+
+        assertEquals(50, (int) debts.get(p2).get(p1));
+
+        assertEquals(50, (int) debts.get(p3).get(p2));
+        assertEquals(100, (int) debts.get(p3).get(p1));
+    }
+
+    @Test
+    void testEquals() {
+        Event e1 = new Event();
+        e1.setId(1L);
+
+        Event e2 = new Event();
+        e2.setId(1L);
+
+        assertEquals(e1, e2);
+    }
 }
